f0c964289947a976c7be1a44e2eb749c851e0f1e
Mohamed Hassan <mohamedhassan@Mohameds-Air.broadband>
2022-11-20 23:12:03 +0000

    add homepage

--- next.config.js
+++ next.config.js
@@ -3,13 +3,10 @@ module.exports = {
   reactStrictMode: true,
   images: {
     domains: ["https://images.unsplash.com/", "https://res.cloudinary.com/"],
-
-    allowFutureImage: true,
-    layoutRaw: true
   },
 }
 
 
-const withBundleAnalyzer = require('@next/bundle-analyzer')({
+module.exports = withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 })
