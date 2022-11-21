574847cad11bd28588be146f51bae0fdaf9bfc6d
Mohamed Hassan <mohamedhassan@Mohameds-Air.broadband>
2022-11-21 21:16:11 +0000

    wire up socials

--- components/footer/index.tsx
+++ components/footer/index.tsx
@@ -5,10 +5,35 @@ const Footer = () => {
   return (
     <footer className="py-12 flex flex-col items-center justify-center">
         <div className="flex items-center justify-center space-x-4">
-        <i className="md:text-3xl"><BsLinkedin/></i>
-        <i className="md:text-3xl"><BsTwitter/></i>
+        <i className="md:text-3xl text-xl">
+        <a
+          href="https://www.linkedin.com/in/mohamedahassan1998/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <BsLinkedin/>
+          </a></i>
+        <i className="md:text-3xl text-xl">
+        <a
+          href="https://twitter.com/momito8_"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <BsTwitter/>
+          </a></i>
         </div>
-        <small className="py-2 md:py-4">Copyright @ 2022 COVERED</small>
+        <small className="pt-3 md:pt-4">
+        Built and designed by {" "}
+        <a
+          href="https://momito.co.uk/"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="font-semibold"
+        >
+          Momito
+        </a></small>
+        <small className="py-2 md:py-2">Copyright @ 2022 COVERED</small>
+
 
     </footer>
   )
