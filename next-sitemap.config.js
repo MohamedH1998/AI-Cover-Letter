9e9652bd6ba0343469f82de877a9c553397af8ab
Mohamed Hassan <mohamedhassan@Mohameds-MacBook-Air.local>
2022-11-29 20:21:42 +0000

    implement sitemaps

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,5 +1,5 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-    siteUrl: process.env.SITE_URL || 'https://cover-letter-ai.netlify.app/',
-    generateRobotsTxt: true, 
-  }
+  siteUrl: 'https://cover-letter-ai.netlify.app/',
+  generateRobotsTxt: true
+}
