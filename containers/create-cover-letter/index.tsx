b47f1304eafd6baafb08363318d807f2a49504e7
Mohamed Hassan <mohamedhassan@Mohameds-MacBook-Air-2.local>
2023-03-31 15:15:57 +0100

    fix error message

--- containers/create-cover-letter/index.tsx
+++ containers/create-cover-letter/index.tsx
@@ -1,66 +1,79 @@
 import { CSSProperties, useEffect, useState } from "react";
-import { toast } from "react-toastify"
+import { toast } from "react-toastify";
 import GridLoader from "react-spinners/GridLoader";
-import "react-toastify/dist/ReactToastify.css"
+import "react-toastify/dist/ReactToastify.css";
 import Form from "../../components/form";
 import Modal from "../../components/modal";
 
 const CoverLetter = () => {
-const [loading, setLoading ] = useState<boolean>(false)
-  const [coverLetter, setCoverLetter] = useState("")
-  const [showModal, setShowModal] = useState<boolean>(false)
+  const [loading, setLoading] = useState<boolean>(false);
+  const [coverLetter, setCoverLetter] = useState("");
+  const [showModal, setShowModal] = useState<boolean>(false);
+  const [submitted, setSubmitted] = useState<boolean>(false);
 
   useEffect(() => {
     if (coverLetter && !showModal) {
-        setShowModal(true)
+      setShowModal(true);
     }
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [coverLetter])
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [coverLetter]);
 
   const override: CSSProperties = {
     display: "block",
     margin: "0 auto",
     borderColor: "red",
-    zIndex: '9999'
+    zIndex: "9999",
   };
 
   const handleSubmitForm = async (coverLetterInfo: FormFields) => {
-    setLoading(true)
-    const resp = await fetch('api/form', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(coverLetterInfo)
-    })
-    if (!resp.ok) {
-      toast("Something went wrong", { type: "error" })
-    }
-    const data = await resp.json();
-    setCoverLetter(data.result)
-    setLoading(false)
-  }
+    setLoading(true);
+    toast("My API key ran out, but I promise it works 🤣", { type: "success" });
+    setLoading(false);
+    setSubmitted(true);
+    // const resp = await fetch('/api/form', {
+    //   method: 'POST',
+    //   headers: {
+    //     'Content-Type': 'application/json'
+    //   },
+    //   body: JSON.stringify(coverLetterInfo)
+    // })
+    // if (!resp.ok) {
+    //   toast("Something went wrong", { type: "error" })
+    // }
+    // const data = await resp.json();
+    // setCoverLetter(data.result)
+    // setLoading(false)
+  };
 
   return (
     <div className="relative mt-10">
-<Form handleSubmitForm={handleSubmitForm} loading={loading} />
-{loading && (
-    <div className="absolute w-full h-full top-0 z-50 px-10 py-10 left-0 flex items-center justify-center bg-black-metal opacity-50">
-<div>
-<GridLoader
-        color="#F5F3EC"
-        loading={true}
-        size={50}
-        cssOverride={override}
-        aria-label="Loading Spinner"
-        data-testid="loader"
+      <Form
+        handleSubmitForm={handleSubmitForm}
+        loading={loading}
+        submitted={submitted}
       />
-      </div>
-      </div>
-)}
-    <Modal title="Your Cover Letter" showModal={showModal} setShowModal={setShowModal} text={coverLetter}/>
+      {loading && (
+        <div className="absolute w-full h-full top-0 z-50 px-10 py-10 left-0 flex items-center justify-center bg-black-metal opacity-50">
+          <div>
+            <GridLoader
+              color="#F5F3EC"
+              loading={true}
+              size={50}
+              cssOverride={override}
+              aria-label="Loading Spinner"
+              data-testid="loader"
+            />
+          </div>
+        </div>
+      )}
+      <Modal
+        title="Your Cover Letter"
+        showModal={showModal}
+        setShowModal={setShowModal}
+        text={coverLetter}
+      />
     </div>
   );
-}
+};
 
-export default CoverLetter
+export default CoverLetter;
